c64dc95efce462e3144ba8edee8eafda200cd912
Niclas Sommer <nsomme15@student.aau.dk>
2017-10-03 00:12:30 -0500

    Extend canvas to cover the footer as well Disabled pointer events in footer besides links

--- src/main/fsm.js
+++ src/main/fsm.js
@@ -108,7 +108,7 @@ window.onload = function() {
 	canvas.id = "canvas";
 	canvas.classList.add('canvas');
 	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight * 0.8;
+	canvas.height = window.innerHeight - 100;
 	
 	document.getElementById('canvasContainer').appendChild(canvas);
 	state.canvas = document.getElementById('canvas');
