619d9ad929d116484f495c5bd6ded1d0888e25e0
Niclas Sommer <nsomme15@student.aau.dk>
2017-10-03 21:09:55 -0500

    Moved to webpack 3

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,43 @@
-var webpack = require("webpack");
-var fs = require("fs");
+const path = require('path');
+const webpack = require('webpack');
+const fs = require("fs");
 
-var license = fs.readFileSync(`./license.md`, "utf8");
+const license = fs.readFileSync(`./license.md`, "utf8");
 
-module.exports = {
-  entry: `./src/main/fsm.js`,
-  output: {
-    path: `./www/`,
-    filename: "fsm.js",
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|www)/,
-        loader: "babel-loader",
-        query: {
-          presets: ["es2015"],
-          plugins: ["transform-object-rest-spread"]
-        }
-      }
-    ]
-  },
-  plugins: [
-    new webpack.BannerPlugin(license, {entryOnly: true})
-  ],
-  devtool: "inline-source-map"
+module.exports = function(env) {
+    return {
+        context: path.resolve(__dirname, 'src'),
+
+        target: 'web',
+
+        entry: {
+            'fsm': './main/fsm.js',
+        },
+
+        output: {
+          path: path.resolve(__dirname, "www"),
+          filename: "fsm.js",
+        },
+
+        module: {
+            rules: [
+              {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                }
+              },
+            ]
+        },
+
+        resolve: {
+            extensions: ['.js', '.vue', 'json'],
+        },
+
+        plugins: [
+          new webpack.BannerPlugin({banner: license, entryOnly: true})
+        ],
+        devtool: "inline-source-map"
+    }
 }
