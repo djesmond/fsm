61c636ee1139ce5950ce2d2a2b1b9d946f033301
Niclas Sommer <nsomme15@student.aau.dk>
2017-10-06 02:17:45 -0500

    Moved primary functions to vue Creatig nodes and links working Moving working Label still to come

--- src/components/CanvasController.vue
+++ src/components/CanvasController.vue
@@ -1,11 +1,186 @@
 <template>
   <div id="canvasContainer">
-    <canvas id="canvas" class="canvas" :width="canvasWidth" :height="canvasHeight"></canvas>
+    <canvas 
+      id="canvas" 
+      class="canvas" 
+      :width="canvasWidth" 
+      :height="canvasHeight"
+      @dblclick="onDoubleClick"
+      @mousemove="onDrag"
+      @mousedown="onMouseDown"
+      @mouseup="onMouseUp"
+      v-on:keyup.8="onDelete">
+    </canvas>
   </div>
 </template>
 <script>
+import fsm from '../util/fsm';
+import mouse from '../util/mouse';
+import { Node } from '../elements/node';
+import { TemporaryLink } from '../elements/temporary_link';
+import { Link } from "../elements/link";
+import { SelfLink } from "../elements/self_link";
+import { StartLink } from "../elements/start_link";
+
 export default {
+  data() {
+    return {
+      fsm: fsm(),
+      canvas: null,
+      context: null,
+      selectedObject: null,
+      movingObject: false,
+      currentLink: null,
+      originalClick: null,
+    }
+  },
   methods: {
+    onMouseDown(e) {
+      const position = mouse.crossBrowserRelativeMousePos(e);
+      this.originalClick = position;
+      const target = this.selectObject(position.x, position.y);
+      // If we hit a node or a link
+      if (target) {
+        this.selectedObject = target;
+        this.movingObject = true;
+      } else { // We hit the canvas
+        this.selectedObject = null;
+      }
+      if (e.shiftKey) {
+        this.currentLink = new TemporaryLink(position, position);
+      }
+
+      this.render();
+    },
+    onMouseUp(e) {
+      this.movingObject = false;
+      if (this.currentLink) {
+        if (!(this.currentLink instanceof TemporaryLink)) {
+          this.selectedObject = this.currentLink;
+          this.fsm.state.links.push(this.currentLink);
+          //resetCaret();
+        }
+        this.currentLink = null;
+        this.render();
+      }
+    },
+    onDoubleClick(e) {
+      const position = mouse.crossBrowserRelativeMousePos(e);
+      const target = this.selectObject(position.x, position.y);
+      if (target) {
+        if (target === this.selectedObject && target instanceof Node) {
+          this.selectedObject.isAcceptState = !this.selectedObject.isAcceptState;
+        }
+      } else {
+        const newNode = this.fsm.createNode(position.x, position.y);
+        this.selectedObject = newNode;
+      }
+      this.render();
+    },
+    onDrag(e) {
+      const position = mouse.crossBrowserRelativeMousePos(e);
+
+      // If we are drawing a new link
+      if (this.currentLink) {
+        // Get the current target
+        let targetNode = this.selectObject(position.x, position.y);
+        // If the target is not a Node
+        if (!(targetNode instanceof Node)) {
+          // Set it to null as we can only have links to nodes
+          targetNode = null;
+        }
+        if (this.selectedObject === null) {
+          // If we have a target Node we create a Link
+          if (targetNode) {
+            this.currentLink = new StartLink(targetNode, this.originalClick);
+          } else {
+            this.currentLink = new TemporaryLink(this.originalClick, position);
+          }
+        } else {
+          // if the target is itself, create a loop
+          if (targetNode === this.selectedObject) {
+            this.currentLink = new SelfLink(this.selectedObject, position);
+          } else if (targetNode) {
+            // Otherwise we create a normal link
+            this.currentLink = new Link(this.selectedObject, targetNode);
+          } else {
+            // Link still hasn't been connected
+            // So we create a new Temp Link with new position
+            // to make it "follow" the mouse
+            this.currentLink = new TemporaryLink(this.selectedObject.closestPointOnCircle(position.x, position.y), position);
+          }
+        }
+        this.render();
+      } else {
+        // Otherwise we check for moving Nodes
+        if (this.movingObject) {
+          this.selectedObject.setAnchorPoint(position.x, position.y);
+          if (this.selectedObject instanceof Node) {
+            this.snapNode(this.selectedObject);
+          }
+          this.render();
+        }
+      }
+    },
+    onDelete() {
+      if (this.selectedObject) {
+        this.fsm.removeNode(this.selectedObject);
+        this.selectedObject = null;
+        this.render()
+      }
+    },
+    selectObject(x, y) {
+      const { nodes, links } = this.fsm.state;
+
+      let target = nodes.find((node) => node.containsPoint(x,y));
+
+      if (!target) {
+        target = links.find((link) => link.containsPoint(x,y));
+      }
+      // Returns undefined if canvas was hit
+      return target;
+    },
+    snapNode(node) {
+      // Snap a nodes position to other nodes
+      this.fsm.state.nodes.forEach((elm) => {
+        if (elm !== node) {
+          if (Math.abs(node.x - elm.x) < 5) {
+            node.x = elm.x;
+          }
+
+          if (Math.abs(node.y - elm.y) < 5) {
+            node.y = elm.y;
+          }
+        }
+      });
+    },
+
+    render() {
+      const { nodes, links } = this.fsm.state;
+      this.context.clearRect(0, 0, canvas.width, canvas.height);
+      this.context.save();
+      this.context.translate(0.5, 0.5);
+
+      nodes.map(node => {
+        this.context.lineWidth = 1;
+        this.context.fillStyle =  this.context.strokeStyle = (node === this.selectedObject) ? 'blue' : 'black';
+        node.draw(this.context);
+      });
+
+      links.map((link) => {
+        this.context.lineWidth = 1;
+        this.context.fillStyle = this.context.strokeStyle = (link === this.selectedObject) ? 'blue' : 'black';
+        link.draw(this.context);
+      });
+
+      if (this.currentLink) {
+        this.context.lineWidth = 1;
+        this.context.fillStyle = this.context.strokeStyle = 'black';
+        this.currentLink.draw(this.context);
+      }
+      this.context.restore();
+    },
+
   },
   computed: {
     canvasWidth() {
@@ -19,8 +194,16 @@ export default {
   },
   mounted() {
     // This needs to be refactored as we progress with moving control to Vue
-    state.canvas = document.getElementById('canvas');
-  },
+    const el = document.getElementById('canvas');
+    this.canvas = el;
+    this.context = el.getContext('2d');
+    document.addEventListener('keyup', (e) => {
+      // Delete key
+      if (e.keyCode === 46) {
+        this.onDelete();
+      }
+    });
+  }
 }
 </script>
 <style lang="scss" scoped>
